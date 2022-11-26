1aff9a7bc261a5511eba1ee2ec06b3c3e45acbc7
snehathadani <snehacopper@gmail.com>
2022-11-26 10:08:04 -0800

    navigating Programmatically

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  onLoadServers(){
+    //complex calculation
+    //need to have an access to router
+    this.router.navigate(['/servers'])
   }
 
 }
