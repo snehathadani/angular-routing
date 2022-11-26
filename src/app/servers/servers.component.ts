70d248f72cdad553a5a6d8ba5f2c1639aa25d950
snehathadani <snehacopper@gmail.com>
2022-11-26 10:43:36 -0800

    routes

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 import { ServersService } from './servers.service';
 
 @Component({
@@ -9,10 +10,17 @@ import { ServersService } from './servers.service';
 export class ServersComponent implements OnInit {
   public servers: {id: number, name: string, status: string}[] = [];
 
-  constructor(private serversService: ServersService) { }
-
+  constructor(private serversService: ServersService,
+      private router: Router,
+      private route: ActivatedRoute)  { }
+//get currently active route which is of type route. It injects the currently active route
   ngOnInit() {
     this.servers = this.serversService.getServers();
+  }
+
+  onReload(){
+    this.router.navigate(['/servers'], {relativeTo: this.route});//ActiveRouter angular knows currently activated route
+    //relative to this route you should navigate and relative to that route all paths should be resolved
   }
 
 }
