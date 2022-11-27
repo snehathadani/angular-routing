c8a0e0429952655e2863286607d5d06c6c2e8ed1
snehathadani <snehacopper@gmail.com>
2022-11-26 16:10:43 -0800

    when u add your own observables

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
+  paramsSubscription: Subscription;
   user: {id: number, name: string};
 //ActivatedRoute object we injected will give us access to the id passed in the url => selectedUser
   constructor(private route: ActivatedRoute) { }
@@ -16,12 +18,16 @@ export class UserComponent implements OnInit {
       id: this.route.snapshot.params['id'], //all the params will be retrievable off the route
       name:  this.route.snapshot.params['name']
     }
-    this.route.params.subscribe(//call subscribe method to observe
+    this.paramsSubscription = this.route.params.subscribe(//call subscribe method to observe
         (params: Params)=> { //params are observable that help u work with async tasks
           this.user.id = params['id'];//1st argument which is a func is executed or fired, when params change
           this.user.name = params['name'];//the fucn has params that change as an 1st argument
         }
     )
+  }
+
+  ngOnDestroy(): void {
+    this.paramsSubscription.unsubscribe();
   }
 
 }
